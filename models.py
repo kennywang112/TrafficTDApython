d3f633ddf54c8be293e86b738e1c590e42baa7ef
Wang chi-chien <kennywang2003@gmail.com>
2024-06-16 13:21:12 +0800

    nice classification

--- models.py
+++ models.py
@@ -1,91 +1,96 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import silhouette_score, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import StandardScaler
+
+from imblearn.combine import SMOTEENN
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+from imblearn.under_sampling import EditedNearestNeighbours, RandomUnderSampler
+
+from sklearn.model_selection import GridSearchCV, train_test_split
+
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import make_pipeline
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
+
+from sklearn.metrics import silhouette_score, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
 def svc_cm_with_grid_search(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    ros = RandomOverSampler(random_state=42)
-    X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
+    smote = SMOTE(random_state=42, k_neighbors=3)
+    enn = EditedNearestNeighbours(n_neighbors=3)
+    smote_enn = SMOTEENN(smote=smote, enn=enn)
+    X_resampled_train, y_resampled_train = smote_enn.fit_resample(X_train, y_train)
 
-    # Set up the parameter grid
+    min_class_count = min(sum(y_test == 1), sum(y_test == 2))
+    
+    rus_test = RandomUnderSampler(sampling_strategy={1: min_class_count, 2: min_class_count}, random_state=42)
+    X_resampled_test, y_resampled_test = rus_test.fit_resample(X_test, y_test)
+
     param_grid = {
         'C': [1],
         'multi_class': ['ovr', 'crammer_singer']
     }
 
-    # Initialize the GridSearchCV object
     grid_search = GridSearchCV(LinearSVC(penalty='l2', dual=True, fit_intercept=True, random_state=42, max_iter=100000), param_grid, cv=5, scoring='accuracy')
     
-    # Fit the grid search to the data
-    grid_search.fit(X_resampled, y_resampled)
+    grid_search.fit(X_resampled_train, y_resampled_train)
     
-    # Extract the best model
     best_model = grid_search.best_estimator_
     
-    # Make predictions
-    y_pred = best_model.predict(X_test)
+    y_pred = best_model.predict(X_resampled_test)
 
-    # Calculate confusion matrix and accuracy
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_resampled_test, y_pred)
+    accuracy = accuracy_score(y_resampled_test, y_pred)
 
-    # Prepare detailed performance metrics
-    cm_df = pd.DataFrame(conf_matrix, index=[f'Actual_{i}' for i in range(conf_matrix.shape[0])], columns=[f'Predicted_{i}' for i in range(conf_matrix.shape[1])])
-    precision = precision_score(y_test, y_pred, average=None, zero_division=0)
-    recall = recall_score(y_test, y_pred, average=None)
-    f1 = f1_score(y_test, y_pred, average=None)
+    precision = precision_score(y_resampled_test, y_pred, average=None, zero_division=0)
+    recall = recall_score(y_resampled_test, y_pred, average=None)
+    f1 = f1_score(y_resampled_test, y_pred, average=None)
 
-    metrics = {
+    metrics_df = pd.DataFrame({
         'Label': [f'Class_{i}' for i in range(len(precision))],
         'Precision': precision,
         'Recall': recall,
         'F1 Score': f1
-    }
-    
-    metrics_df = pd.DataFrame(metrics)
-    
+    })
+
     return metrics_df, accuracy, conf_matrix
 
 def logistic_cm_gridsearch(X, y):
-    # 分割資料集
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    # 使用RandomOverSampler來平衡資料
-    ros = RandomOverSampler(random_state=42)
-    X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
-
-    # 建立邏輯回歸模型並使用GridSearchCV來找到最佳參數
+    # smote_enn = SMOTEENN(random_state=42)
+    # X_resampled_train, y_resampled_train = smote_enn.fit_resample(X_train, y_train)
+    smote = SMOTE(random_state=42, k_neighbors=3)
+    enn = EditedNearestNeighbours(n_neighbors=3)
+    smote_enn = SMOTEENN(smote=smote, enn=enn)
+    X_resampled_train, y_resampled_train = smote_enn.fit_resample(X_train, y_train)
+    
+    min_class_count = min(sum(y_test == 1), sum(y_test == 2))
+    
+    rus_test = RandomUnderSampler(sampling_strategy={1: min_class_count, 2: min_class_count}, random_state=42)
+    X_resampled_test, y_resampled_test = rus_test.fit_resample(X_test, y_test)
+    
     model = LogisticRegression(solver='saga', multi_class='multinomial', max_iter=1000)
+    
     parameters = {
         'penalty': ['l1', 'l2'],
-        'C': [0.1, 1, 10]
+        'C': [0.01, 0.1, 1, 10, 100]
     }
+
     grid_search = GridSearchCV(model, parameters, cv=5, scoring='accuracy')
-    grid_search.fit(X_resampled, y_resampled)
-
-    # 使用最佳模型進行預測
+    grid_search.fit(X_resampled_train, y_resampled_train)
     best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
 
-    # 生成並打印混淆矩陣和各項度量指標
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average=None, zero_division=0)
-    recall = recall_score(y_test, y_pred, average=None)
-    f1 = f1_score(y_test, y_pred, average=None)
+    y_pred = best_model.predict(X_resampled_test)
+
+    conf_matrix = confusion_matrix(y_resampled_test, y_pred)
+    accuracy = accuracy_score(y_resampled_test, y_pred)
+
+    precision = precision_score(y_resampled_test, y_pred, average=None, zero_division=0)
+    recall = recall_score(y_resampled_test, y_pred, average=None)
+    f1 = f1_score(y_resampled_test, y_pred, average=None)
 
     metrics_df = pd.DataFrame({
         'Label': [f'Class_{i}' for i in range(len(precision))],
@@ -97,20 +102,24 @@ def logistic_cm_gridsearch(X, y):
     return metrics_df, accuracy, conf_matrix
 
 def rf_with_gridsearch(X, y):
-    # Splitting the dataset
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Handling class imbalance
-    ros = RandomOverSampler(random_state=42)
-    X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
+    smote = SMOTE(random_state=42, k_neighbors=3)
+    enn = EditedNearestNeighbours(n_neighbors=3)
+    smote_enn = SMOTEENN(smote=smote, enn=enn)
+    X_resampled_train, y_resampled_train = smote_enn.fit_resample(X_train, y_train)
     
-    # Scaling features
+    min_class_count = min(sum(y_test == 1), sum(y_test == 2))
+    
+    rus_test = RandomUnderSampler(sampling_strategy={1: min_class_count, 2: min_class_count}, random_state=42)
+    X_resampled_test, y_resampled_test = rus_test.fit_resample(X_test, y_test)
+    
     scaler = StandardScaler()
-    X_resampled_scaled = scaler.fit_transform(X_resampled)
-    X_test_scaled = scaler.transform(X_test)
+    X_resampled_train_scaled = scaler.fit_transform(X_resampled_train)
+    X_resampled_test_scaled = scaler.transform(X_resampled_test)
 
-    # Defining the model and parameters for grid search
-    dt_model = RandomForestClassifier(random_state=43)
+    model = RandomForestClassifier(random_state=43)
+    
     param_grid = {
         'n_estimators': [300],
         'max_features': ['sqrt', 'log2'],
@@ -119,23 +128,18 @@ def rf_with_gridsearch(X, y):
         'min_samples_leaf': [1]
     }
 
-    # Grid search
-    grid_search = GridSearchCV(estimator=dt_model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
-    grid_search.fit(X_resampled_scaled, y_resampled)
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+    grid_search.fit(X_resampled_train_scaled, y_resampled_train)
+    best_model = grid_search.best_estimator_
 
-    # Predicting labels for the test set
-    y_pred = grid_search.predict(X_test_scaled)
-
-    # Calculating the confusion matrix
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
+    y_pred = best_model.predict(X_resampled_test_scaled)
     
-    # Formatting the confusion matrix for display
-    cm_df = pd.DataFrame(conf_matrix, index=[f'Actual_{i}' for i in range(conf_matrix.shape[0])], 
-                         columns=[f'Predicted_{i}' for i in range(conf_matrix.shape[1])])
-    precision = precision_score(y_test, y_pred, average=None, zero_division=0)
-    recall = recall_score(y_test, y_pred, average=None)
-    f1 = f1_score(y_test, y_pred, average=None)
+    conf_matrix = confusion_matrix(y_resampled_test, y_pred)
+    accuracy = accuracy_score(y_resampled_test, y_pred)
+
+    precision = precision_score(y_resampled_test, y_pred, average=None, zero_division=0)
+    recall = recall_score(y_resampled_test, y_pred, average=None)
+    f1 = f1_score(y_resampled_test, y_pred, average=None)
 
     metrics = {
         'Label': [f'Class_{i}' for i in range(len(precision))],
